52eb79d5ebe595f210ed0a29391877dd9e1b49d1
bryanc.n <bryan.chuinkam@gmail.com>
2023-05-25 00:22:04 +0000

    added functionality to fetch tweets from server and used timeago library to get num of days since tweet posted

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,34 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Test / driver code (temporary). Eventually will get this from the server.
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 const renderTweets = function(tweets) {
   for (const tweetobj of tweets) {
     const tweet = createTweetElement(tweetobj);
@@ -53,7 +25,7 @@ const createTweetElement = function(tweetData) {
                     </header>
                     <p class="posted-tweet-text"><strong>${tweetData.content.text}</strong> </p>
                     <footer class="tweet-article-footers">
-                      <span class="tweet-article-footer-left">${tweetData.created_at}</span>
+                      <span class="tweet-article-footer-left">${timeago.format(tweetData.created_at)}</span>
                       <div class="tweet-article-footer-right">
                         <i class="fa-solid fa-flag"></i>
                         <i class="fa-solid fa-retweet"></i>
@@ -65,9 +37,16 @@ const createTweetElement = function(tweetData) {
 };
 
 
-
 $(document).ready(function() {
-  renderTweets(data);
+
+  const loadTweets = function() {
+    $.ajax({ type: "GET", url: "/tweets", dataType: 'JSON' })
+      .then(function(response) {
+        renderTweets(response);
+      });
+  };
+  loadTweets();
+
 
   $("#submit-tweet-form").on("submit", function(event) {
     event.preventDefault();
@@ -76,9 +55,8 @@ $(document).ready(function() {
     const actionUrl = $(this).attr("action");
     const formMethod = $(this).attr("method");
 
-    $.ajax({type: formMethod, url: actionUrl, data: formValues,}).then(function(response) {
-      console.log("response: ", response);
-    });
+    $.ajax({ type: formMethod, url: actionUrl, data: formValues, })
+      .then(function(response) { console.log('Success: '); });
   });
 
 });
